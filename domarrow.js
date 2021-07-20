5c4ec92892308f10b32a9f628dbbe3093e84eb83
Funlang <info@funlang.org>
2021-07-20 13:49:30 +0800

    Update domarrow.js

--- domarrow.js
+++ domarrow.js
@@ -224,8 +224,8 @@ window.DOMLineArrow = (panel) => {
         tag.ob_refcount -= 1;
         connectionObserver.observe(tag, { attributeFilter: [] });
         repaintConnection(tag);
-        tab.ob_refcount += 1;
-        tab.ob_refcount > 0 && connectionObserver.observe(tag, { attributes: true, childList: true, subtree: true });
+        tag.ob_refcount += 1;
+        tag.ob_refcount > 0 && connectionObserver.observe(tag, { attributes: true, childList: true, subtree: true });
     }
 
     function createOne(newElement) {
